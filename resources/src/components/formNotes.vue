4184811914c3d5b027f3efe40fcfb6f86da28101
vhico <vhico pratama>
2020-10-21 15:42:01 +0700

    Second commit

---
+++ resources/src/components/formNotes.vue
@@ -0,0 +1,106 @@
+<template>
+    <div class="formNotes">
+        <form @submit="submitNote">
+            <div class="menu">
+                <button type="button" @click="submitRemove" class="bg-danger btn btn-delete">Delete</button>
+                <button type="submit" class="bg-success btn">Save</button>            
+            </div>
+
+            <div class="content">
+                <input type="text" class="text" placeholder="id" v-model="id">
+                <input type="text" class="text" placeholder="Title" v-model="title">
+                <textarea class="text textarea" placeholder="Tuliskan rencana anda" v-model="description"></textarea>
+
+            </div>
+        </form>
+
+    </div>
+</template>
+
+<script type="text/javascript">
+
+    export default {
+        name: 'formNotes',
+        props: {
+            propSaveNote : {
+                type: Function
+            },
+            propUpdateNote : {
+                type: Function
+            },
+            propRemoveNote : {
+                type: Function
+            }
+        },
+        data: function () {
+            return {
+              id: 0,
+              title: '',
+              description: ''  
+            }
+        },
+        methods: {
+            submitNote(e){
+                e.preventDefault();
+
+                if(this.id === 0){
+                    this.propSaveNote(this.title, this.description);
+                }
+                else{
+                    this.propUpdateNote(this.title, this.description);
+                }
+            },
+            submitRemove(){
+                this.propRemoveNote(this.id);
+                this.resetInput();
+            },
+            resetInput(){
+                this.id = 0;
+                this.title = '';
+                this.description = '';
+            }
+        },
+        mounted(){
+                this.$root.$on('emitForm', data => {
+                    console.log(data);
+                    this.id =  data.id;
+                    this.title =  data.title;
+                    this.description =  data.description;
+                })
+        }
+    }
+</script>
+<style scoped>
+.menu{
+     background: #f7f7f7;
+     padding:10px 25px;
+     margin-bottom: 25px;
+     text-align:right;
+     border-bottom: 1px solid #e8e6e6;
+}
+.btn-delete{ margin-right:10px; }
+.content{
+     padding: 0px 25px;
+}
+.text{
+     display: block;
+     width: 100%;
+     padding: 0px;
+     font-size: 20px;
+     font-weight: bold;
+     color: #2c3e50;
+     border: none;
+     margin-bottom: 10px;
+     box-sizing: border-box;
+     outline: none;
+}
+.textarea{
+     min-height: 350px;
+     font-size: 15px;
+     font-weight: lighter;
+     line-height: 30px;
+}
+.loader{
+     vertical-align: middle;
+}
+</style>
---
+++ resources/src/components/listNotes.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(row, index) in notes" :key="index">
+                <button class="btn-note" @click="editNote(row.id)">
+                    <label>{{ row.title }}</label>
+                    <span>{{ row.description }}</span>
+                </button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script type="text/javascript">
+
+    export default {
+       name: 'listNotes',
+       data: function () {
+           return {
+                notes : [{ id:1, title: 'wenote', description: 'Ini isi wenotes' }, { id:2,  title: 'Super user', description: 'Ini isi super user'  }] //{ id:1, title: 'wenote', description: 'Ini isi wenotes' }, { id:2,  title: 'Super user', description: 'Ini isi super user'  }
+           }
+       },
+        props: {
+            propNotes : {
+                type: Array
+            },
+            propEditNote : {
+                type: Function
+            }
+        },
+        methods:{
+            editNote(id){
+                let dataForm = this.notes.find(note => note.id === id);
+                this.$root.$emit('emitForm', dataForm);
+            },
+        }
+    }
+</script>
+
+<style>
+ul {
+    list-style-type: none;
+    padding: 0;
+    margin:0px;
+}
+.btn-note {
+    text-align: left;
+    border: none;
+    border-bottom: 1px solid gainsboro;
+    padding: 0px 15px;
+    cursor: pointer;
+    outline: none;
+    background: #f7f7f7;
+    height: 150px;
+    width: 100%;
+}
+.btn-note:hover {
+    background:#eaeaea;
+}
+.btn-note label{
+    display:block;
+    color: #444444;
+    font-size: 1.5em;
+    margin-bottom: 15px;
+}
+.btn-note span{
+   color: #545454;
+}
+</style>
